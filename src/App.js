17e73085e402e947cf63a0f1b71932f6242992c4
Bart Hendrix <info@barthendrix.com>
2019-10-06 15:46:13 +0200

    background animation is in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Dock from './Dock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/style.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <main className="main">
+          <h1>Animated dock</h1>
+          <p>
+            Originally built in flash in 2007, this version uses react-motion.
+          </p>
+          <Dock />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
